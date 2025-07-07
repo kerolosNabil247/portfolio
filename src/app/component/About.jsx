3e9cffd121ddaa28de8f6e4102cf47b9d65b6c7d
Kerolos Nabil <kerolosnabil247@gmail.com>
2025-07-07 16:41:29 +0300

    feat(hero): initial hero section

---
+++ src/app/component/About.jsx
@@ -0,0 +1,45 @@
+
+
+export default function About() {
+  return (
+    <div>
+        <h1 id="about">About</h1>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In exercitationem non corporis magni quaerat repudiandae molestias reiciendis recusandae dolor est quis mollitia aliquam accusantium dolores, architecto iure autem doloribus et!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In exercitationem non corporis magni quaerat repudiandae molestias reiciendis recusandae dolor est quis mollitia aliquam accusantium dolores, architecto iure autem doloribus et!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In exercitationem non corporis magni quaerat repudiandae molestias reiciendis recusandae dolor est quis mollitia aliquam accusantium dolores, architecto iure autem doloribus et!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In exercitationem non corporis magni quaerat repudiandae molestias reiciendis recusandae dolor est quis mollitia aliquam accusantium dolores, architecto iure autem doloribus et!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In exercitationem non corporis magni quaerat repudiandae molestias reiciendis recusandae dolor est quis mollitia aliquam accusantium dolores, architecto iure autem doloribus et!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In exercitationem non corporis magni quaerat repudiandae molestias reiciendis recusandae dolor est quis mollitia aliquam accusantium dolores, architecto iure autem doloribus et!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In exercitationem non corporis magni quaerat repudiandae molestias reiciendis recusandae dolor est quis mollitia aliquam accusantium dolores, architecto iure autem doloribus et!</p>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet consectetur tenetur error ea distinctio libero expedita sit velit facere? Eius, magnam? Cum ipsum numquam dolor molestiae at labore mollitia voluptate?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet consectetur tenetur error ea distinctio libero expedita sit velit facere? Eius, magnam? Cum ipsum numquam dolor molestiae at labore mollitia voluptate?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet consectetur tenetur error ea distinctio libero expedita sit velit facere? Eius, magnam? Cum ipsum numquam dolor molestiae at labore mollitia voluptate?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet consectetur tenetur error ea distinctio libero expedita sit velit facere? Eius, magnam? Cum ipsum numquam dolor molestiae at labore mollitia voluptate?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet consectetur tenetur error ea distinctio libero expedita sit velit facere? Eius, magnam? Cum ipsum numquam dolor molestiae at labore mollitia voluptate?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet consectetur tenetur error ea distinctio libero expedita sit velit facere? Eius, magnam? Cum ipsum numquam dolor molestiae at labore mollitia voluptate?</p>
+    </div>
+  )
+}
